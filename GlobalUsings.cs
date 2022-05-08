b668ad716d7801a867f7d7f27e8d9d19b75c06e4
Chris Barr <umo2636@gmail.com>
2022-05-08 17:42:36 -0400

    stuff

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,4 @@
 global using System;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Configuration.Json;
 global using Telegram.Bot;
--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
 var config = builder.Build();
 var connectionString = config["ConnectionString"];
 
-var botClient = new TelegramBotClient(connectionString);
+// var botClient = new TelegramBotClient(connectionString);
 
-var me = botClient.GetMeAsync().Result;
-Console.WriteLine("Hello! My name is {0}", me.FirstName);
+// var me = botClient.GetMeAsync().Result;
+Console.WriteLine(connectionString);
+
+// Console.WriteLine("Hello! My name is {0}", me.FirstName);
