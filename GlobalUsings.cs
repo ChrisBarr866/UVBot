a88d76fbd0b782dcf2f0b3a9b60176e21856075d
Chris Barr <umo2636@gmail.com>
2022-05-08 22:56:54 -0400

    Added UV api

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,3 +2,5 @@ global using System;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Configuration.Json;
 global using Telegram.Bot;
+global using System.Net;
+global using Newtonsoft.Json;
--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
-﻿var builder = new ConfigurationBuilder()
+﻿// get current directory
+
+var builder = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
 .AddJsonFile("appsettings.json", true, true);
 
 var config = builder.Build();
-var connectionString = config["ConnectionString"];
 
-// var botClient = new TelegramBotClient(connectionString);
+var connectionString = config["TelegramBotKey"];
 
-// var me = botClient.GetMeAsync().Result;
+var botClient = new TelegramBotClient(connectionString);
+
+var me = botClient.GetMeAsync().Result;
 Console.WriteLine(connectionString);
 
-// Console.WriteLine("Hello! My name is {0}", me.FirstName);
+var client = new HttpClient();
+var httpRequestMessage = new HttpRequestMessage
+{
+        Method = HttpMethod.Get,
+        RequestUri = new Uri("https://api.openuv.io/api/v1/uv"),
+        Headers = { 
+            { "x-access-token", config["UVApiKey"] },
+        },
+    };
+
+var response = client.SendAsync(httpRequestMessage).Result;
+var responseBody = response.Content.ReadAsStringAsync().Result;
+
+Console.WriteLine(responseBody);
+Console.WriteLine("Hello! My name is {0}", me.FirstName);
